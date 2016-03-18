a869cf1c323abfea6e41cadec53ac1bd63e68278
Grant Bartlett <hello@grant-bartlett.com>
2016-03-18 11:45:21 +0000

    finishing up signup

--- spec/requests/users_signups_spec.rb
+++ spec/requests/users_signups_spec.rb
@@ -1,34 +1,4 @@
 require "rails_helper"
-#
-# RSpec.describe "User Signups", :type => :request do
-#
-#   it "should not allow invalid invalid signup parameters" do
-#     before_count = User.count
-#     get "/users/new"
-#     expect(response).to render_template(:new)
-#
-#     post "/users/", user: { name:  "",
-#       email: "user@invalid",
-#       password:              "foo",
-#       password_confirmation: "bar" }
-#
-#       after_count  = User.count
-#       expect(before_count).to eq(after_count)
-#     end
-#
-#
-#     it "should create a User and redirect to the Users page" do
-#       get "/users/new"
-#       expect(response).to render_template(:new)
-#
-#       post "/users/", user: { name:  "User",
-#         email: "user@example.com",
-#         password:              "foobar",
-#         password_confirmation: "foobar" }
-#         expect(response).to have_http_status(:created)
-#       end
-#     end
-
 
 RSpec.describe "User signups", :type => :request do
 
@@ -42,10 +12,11 @@ RSpec.describe "User signups", :type => :request do
   it "allows valid parameters in a sign up form" do
     get "/users/new"
     expect(response).to render_template(:new)
-    post "/users", :user => {:name => "Grant", :email => "grant@smswmedia.com", :password => "foobar", :password_confirmation => "foobar"}
-    expect(response).to redirect_to(assigns(:user))
-    follow_redirect!
-    expect(response).to render_template(:show)
+    post "/users", :user => {:name => "Grant", :email => "grant@smswmedia.com",
+      :password => "foobar", :password_confirmation => "foobar"}
+      expect(response).to redirect_to(assigns(:user))
+      follow_redirect!
+      expect(response).to render_template(:show)
   end
 
 end
