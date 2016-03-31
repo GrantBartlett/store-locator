0486cf7329969e8d4fe0b006f15a7566442fe7b7
GrantBartlett <hello@grant-bartlett.com>
2016-03-31 13:21:28 +0100

    fixing user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,53 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before(:all) do
-    @user = User.new(name: "Grant", email: "grant@smswmedia.com", password: "FooBar", password_confirmation: "foobar")
+
+  before(:each) do
+    @test_user = create(:user, password: 'password')
   end
 
   describe "user object" do
     it "should be valid" do
-      expect(@user).to be_valid
+      expect(@test_user).to be_valid
     end
   end
 
   describe "name field" do
     it "should not be left blank" do
-      expect(@user.name).not_to be_empty
+      expect(@test_user.name).not_to be_empty
     end
   end
 
   describe "email field" do
     it "should not be left blank" do
-      expect(@user.email).not_to be_empty
+      expect(@test_user.email).not_to be_empty
     end
   end
 
   describe "email address" do
     it "should be unique upon saving to the database" do
-      duplicate_user = @user.dup
-      duplicate_user.email = @user.email.upcase
-      @user.save
+      duplicate_user = @test_user.dup
+      duplicate_user.email = @test_user.email.upcase
+      @test_user.save
       expect(duplicate_user).not_to be_valid
     end
 
     it "should not be mixed case upon saving to the database" do
       mixed_case_email = "gRaNt@smswmedia.com"
-      @user.email = mixed_case_email
-      @user.save
-      expect(@user.email).not_to eq(mixed_case_email)
+      @test_user.email = mixed_case_email
+      @test_user.save
+      expect(@test_user.email).not_to eq(mixed_case_email)
     end
   end
 
   describe "password" do
     it "should not be left blank" do
-      @user.password = @user.password_confirmation = " " * 6
-      expect(@user).to_not be_valid
+      @test_user.password = @test_user.password_confirmation = " " * 6
+      expect(@test_user).to_not be_valid
     end
 
     it "should have a minimum length" do
-      @user.password = @user.password_confirmation = "a" * 5
-      expect(@user).to_not be_valid
+      @test_user.password = @test_user.password_confirmation = "a" * 5
+      expect(@test_user).to_not be_valid
     end
   end
 end
--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  describe "POST session#new" do
+    it "rejects invalid login credentials" do
+      get '/login'
+      render_template 'sessions/new'
+      post '/login', session: { email: '', password: '' }
+      expect(response).to render_template(:new)
+    end
 
-  it "rejects invalid login credentials" do
-    get "/login"
-    render_template 'sessions/new'
-    post "/login", session: { email: "", password: "" }
-    render_template 'sessions/new'
-  end
-
-  it "accepts valid login credentials" do
-    get "/login"
-    user = create(:user, password: "password")
-    post "/login", session: { email: user.email, password: 'password'}
-    redirect_to user
-    follow_redirect!
-    render_template 'users/show'
+    it "accepts valid login credentials" do
+      get '/login'
+      user = create(:user, password: 'password')
+      post '/login', session: { email: user.email, password: 'password'}
+      redirect_to user
+      follow_redirect!
+      expect(response).to render_template(:show)
+    end
   end
 end
