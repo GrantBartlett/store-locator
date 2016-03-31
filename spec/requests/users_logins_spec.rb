368625668e61f5f11b858c77983e33637a664c12
GrantBartlett <hello@grant-bartlett.com>
2016-03-31 13:00:03 +0100

    first passed test for signups

--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
 
-  it "will NOT be able to login with INVALID credentials" do
+  it "rejects invalid login credentials" do
     get "/login"
     render_template 'sessions/new'
     post "/login", session: { email: "", password: "" }
     render_template 'sessions/new'
   end
 
-  it "will be able to login with valid credentials" do
+  it "accepts valid login credentials" do
     get "/login"
     user = create(:user, password: "password")
     post "/login", session: { email: user.email, password: 'password'}
