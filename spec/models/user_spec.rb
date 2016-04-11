42b7a05ec30cc9ae23ce463cd1deab6a7a451240
Grant Bartlett <hello@grant-bartlett.com>
2016-04-11 09:57:40 +0100

    user model spec changing :before to let

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,54 +1,51 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe "User", type: :model do
+  let(:test_user) { create(:user, password: 'password') }
 
-  before(:each) do
-    @test_user = create(:user, password: 'password')
-  end
-
-  describe "user object" do
-    it "should be valid" do
-      expect(@test_user).to be_valid
+  context 'user object' do
+    it 'should be valid' do
+      expect(test_user).to be_valid
     end
   end
 
-  describe "name field" do
-    it "should not be left blank" do
-      expect(@test_user.name).not_to be_empty
+  context 'name field' do
+    it 'should not be left blank' do
+      expect(test_user.name).not_to be_empty
     end
   end
 
-  describe "email field" do
-    it "should not be left blank" do
-      expect(@test_user.email).not_to be_empty
+  context 'email field' do
+    it 'should not be left blank' do
+      expect(test_user.email).not_to be_empty
     end
   end
 
-  describe "email address" do
-    it "should be unique upon saving to the database" do
-      duplicate_user = @test_user.dup
-      duplicate_user.email = @test_user.email.upcase
-      @test_user.save
+  context 'email address' do
+    it 'should be unique when saving' do
+      duplicate_user = test_user.dup
+      duplicate_user.email = test_user.email.upcase
+      test_user.save
       expect(duplicate_user).not_to be_valid
     end
 
-    it "should not be mixed case upon saving to the database" do
-      mixed_case_email = "gRaNt@smswmedia.com"
-      @test_user.email = mixed_case_email
-      @test_user.save
-      expect(@test_user.email).not_to eq(mixed_case_email)
+    it 'should not be mixed case when saving' do
+      mixed_case_email = 'gRaNt@smswmedia.com'
+      test_user.email = mixed_case_email
+      test_user.save
+      expect(test_user.email).not_to eq(mixed_case_email)
     end
   end
 
-  describe "password" do
-    it "should not be left blank" do
-      @test_user.password = @test_user.password_confirmation = " " * 6
-      expect(@test_user).to_not be_valid
+  context 'password' do
+    it 'should not be left blank' do
+      test_user.password = test_user.password_confirmation = " " * 6
+      expect(test_user).to_not be_valid
     end
 
-    it "should have a minimum length" do
-      @test_user.password = @test_user.password_confirmation = "a" * 5
-      expect(@test_user).to_not be_valid
+    it 'should have a minimum length' do
+      test_user.password = test_user.password_confirmation = "a" * 5
+      expect(test_user).to_not be_valid
     end
   end
 end
