64bb1c34dd9ed037f6997dda4cdf7a66301f1fcd
Grant Bartlett <hello@grant-bartlett.com>
2016-04-15 16:17:46 +0100

    locations adjustments

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,58 +1,55 @@
 class LocationsController < ApplicationController
+  skip_before_action :require_user, only: [:show]
+  skip_before_action :require_admin, only: [:show]
   before_action :set_brand
 
   def index
-    @locations = @brand.locations.all
+    @locations = @brand.locations
     render json: @locations
+  end
+
+  def create
+    @location = @brand.locations.new(location_params)
+    if @location.save
+      redirect_to @brand
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @location.update_attributes(location_params)
+      redirect_to @brand
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @location = @brand.locations.find(params[:id])
+    if @location.destroy
+      @location.destroy
+      redirect_to @brand
+    end
+  end
+
+  def show
   end
 
   def new
     @location = Location.new
   end
 
-  def create
-    if is_admin?
-      @location = @brand.locations.new(location_params)
-      respond_to do |f|
-        if @brand.save
-          f.html { redirect_to @brand, status: :created }
-          f.json { render :show, status: :created, location: @brand }
-        else
-          f.html { redirect_to @brand, status: :unprocessable_entity }
-          f.json { render json: @brand.errors, status: :unprocessable_entity }
-        end
-      end
-    else
-      head 401
-    end
+  def edit
   end
-
-  def destroy
-    if is_admin?
-      @location = @brand.locations.find(params[:id])
-      if @location.destroy
-        respond_to do |f|
-          if @location.save
-            f.html { redirect_to @brand, status: :ok }
-            f.json { render :show, status: :ok, location: @brand }
-          else
-            f.html { redirect_to @brand, status: :bad_request }
-            f.json { render json: @brand.errors, status: :bad_request }
-          end
-        end
-      else
-      end
-    else
-      head 401
-    end
-  end
-
 
   private
+  # Use callbacks to share common setup or constraints between actions.
   def set_brand
-    @brand = Brand.find(params[:brand_id])
+    @brand = Brand.find Brand.decrypt(params[:brand_id])
   end
 
+  # Never trust parameters from the scary internet, only allow the white list through.
   def location_params
     params[:location].permit([:name, :lat, :lng, :content, :url])
   end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,4 @@
 class Location < ActiveRecord::Base
-  # params[:location].permit([:name, :lat, :lng, :content, :url])
   validates :name, presence: true
   validates :lat, presence: true
   validates :lng, presence: true
