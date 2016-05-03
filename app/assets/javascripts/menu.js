ee43f336ea673278735f1a3d1b8e730ae53933e7
Grant Bartlett <hello@grant-bartlett.com>
2016-05-03 12:05:37 +0100

    restore app and correct object heroku complains about

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,12 +1,12 @@
-// $(function(){
-//   $('.hamburger-menu').on('click', function(){
-//     $('.bar').toggleClass('animate');
-//     toggleMenu('.js_menu');
-//   });
-// });
-//
-// function toggleMenu(selector){
-//   $(function(){
-//     $(selector).toggleClass('hidden');
-//   })
-// };
+$(function(){
+  $('.hamburger-menu').on('click', function(){
+    $('.bar').toggleClass('animate');
+    toggleMenu('.js_menu');
+  });
+});
+
+function toggleMenu(selector){
+  $(function(){
+    $(selector).toggleClass('hidden');
+  })
+};
